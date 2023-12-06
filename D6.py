85b514defde0df8970e234982ab60baba4e5b3ad
Stasia <anastasia.saprunova@gmail.com>
2023-12-06 11:59:53 +0100

    Finished PART 2

--- D6.py
+++ D6.py
@@ -1,27 +1,22 @@
-file = open("testInput.txt", "r")
+file = open("input.txt", "r")
 
 tmp = file.readline().split(":")[1].strip().split(" ")
-time = [x for x in tmp if x.isdigit()]
+time = ''
+for elem in tmp:
+    if elem.isdigit():
+        time += elem 
 
 tmpDist = file.readline().split(":")[1].strip().split(" ")
-records = [x for x in tmpDist if x.isdigit()]
+record = ''
+for elem in tmpDist:
+    if elem.isdigit():
+        record += elem 
 
-races = []
-for i in range(len(time)):
-    races.append((int(time[i]), int(records[i])))
+total = 0
 
-total = 1
-
-# PART 1: Brute-force approach
-for race in races:
-    time = race[0]
-    record = race[1]
-    curr_distance = 0
-    amount_wins = 0
-    for ms in range(time):
-        curr_distance = ms * (time - ms)
-        if(curr_distance > record):
-            amount_wins += 1
-    total *= amount_wins
+# PART 2: Brute-force approach
+for i in range(int(time)):
+    if i * (int(time) - i) > int(record):
+        total += 1
 
 print(total)
